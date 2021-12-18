0fbe04cd27ee75e2cdd825f8e89f62dcdaca8e87
Tyler Kindy <tylerkindy@gmail.com>
2021-12-18 09:58:56 -0500

    Add children helpers

--- src/com/tylerkindy/readitlater/rss.clj
+++ src/com/tylerkindy/readitlater/rss.clj
@@ -1,15 +1,15 @@
 (ns com.tylerkindy.readitlater.rss
   (:require [clojure.data.xml :as xml]))
 
-(defn get-channel [feed]
-  (-> feed
-      :content
-      first))
-
-(defn get-str [element tag]
+(defn get-children [element tag]
   (->> element
        :content
-       (filter (fn [element] (and map? (= (:tag element) tag))))
-       first
+       (filter (fn [element] (and map? (= (:tag element) tag))))))
+
+(defn get-child [element tag]
+  (first (get-children element tag)))
+
+(defn get-str [element tag]
+  (->> (get-child element tag)
        :content
        first))
--- test/com/tylerkindy/readitlater/rss_test.clj
+++ test/com/tylerkindy/readitlater/rss_test.clj
@@ -3,8 +3,6 @@
             [clojure.test :refer [deftest is]]
             [clojure.data.xml :as xml]))
 
-(def marco-feed (xml/parse (java.io.FileReader. "test/examples/marco-org.xml")))
-
 (def empty-feed
   (xml/sexp-as-element
    [:rss {:version "2.0"}
@@ -13,8 +11,33 @@
      [:link "https://blog.example.com"]
      [:description "This is a blog with no posts"]]]))
 
+(def empty-channel (r/get-child empty-feed :channel))
+
+(deftest get-children
+  (is (= (r/get-children empty-feed :channel)
+         (list (xml/sexp-as-element
+                [:channel
+                 [:title "Empty blog"]
+                 [:link "https://blog.example.com"]
+                 [:description "This is a blog with no posts"]]))))
+  (is (= (r/get-children (first (r/get-children empty-feed :channel)) :link)
+         (list (xml/sexp-as-element
+                [:link "https://blog.example.com"])))))
+
+(deftest get-child
+  (is (= (r/get-child empty-feed :channel)
+         (xml/sexp-as-element
+          [:channel
+           [:title "Empty blog"]
+           [:link "https://blog.example.com"]
+           [:description "This is a blog with no posts"]])))
+  (is (= (-> empty-feed
+             (r/get-child :channel)
+             (r/get-child :title))
+         (xml/sexp-as-element [:title "Empty blog"]))))
+
 (deftest get-str
-  (is (= (r/get-str (r/get-channel marco-feed) :title)
-         "Marco.org"))
-  (is (= (r/get-str (r/get-channel empty-feed) :link)
+  (is (= (r/get-str empty-channel :title)
+         "Empty blog"))
+  (is (= (r/get-str empty-channel :link)
          "https://blog.example.com")))
