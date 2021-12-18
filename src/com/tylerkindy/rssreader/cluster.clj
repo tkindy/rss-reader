d1456e6dde01d12b978009ea7a08c951d8aaeba2
Tyler Kindy <tylerkindy@gmail.com>
2021-12-18 17:21:39 -0500

    Build trigram index

--- src/com/tylerkindy/rssreader/cluster.clj
+++ src/com/tylerkindy/rssreader/cluster.clj
@@ -9,21 +9,23 @@
        read-string
        (map-indexed (fn [id item] (assoc item :id id)))))
 
-(def split-regex #"[\s,.\"]")
+(def split-regex #"[\s,.\":]")
 (def stopwords (->> (slurp "data/stopwords.txt")
                     str/split-lines
                     (filter (comp not str/blank?))
                     set))
 
+(defn extract-text [html]
+  (-> html
+      Jsoup/parse
+      .body
+      .text))
+
 (defn extract-terms [html]
-  (let [text (-> html
-                 Jsoup/parse
-                 .body
-                 .text)]
-    (->> (str/split text split-regex)
-         (filter (comp not str/blank?))
-         (map str/lower-case)
-         (filter (comp not stopwords)))))
+  (->> (str/split (extract-text html) split-regex)
+       (filter (comp not str/blank?))
+       (map str/lower-case)
+       (filter (comp not stopwords))))
 
 (defn build-term-index [items]
   (->> items
@@ -33,6 +35,27 @@
                       (map (fn [term] {term #{id}})))))
        (apply merge-with set/union)))
 
+(defn extract-trigrams [html]
+  (->> (str/split (extract-text html) split-regex)
+       (filter (comp not str/blank?))
+       (partition 3 1)))
+
+(defn build-trigram-index [items]
+  (->> items
+       (mapcat (fn [{:keys [id description]}]
+                 (->> description
+                      extract-trigrams
+                      (map (fn [trigram] {trigram #{id}})))))
+       (apply merge-with set/union)))
+
 (defn -main [in]
   (let [items (read-items in)
         term-index (build-term-index items)]))
+
+(comment
+  (->> "output/items.edn"
+       read-items
+       build-term-index
+       (map (fn [[term ids]] [term (count ids)]))
+       (sort-by (fn [[_ count]] (* -1 count)))
+       (take 25)))
